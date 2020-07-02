40597bee5effd8a5b73931b9dfc6e7c24e482390
suriya-ganesh <suriyaganesh097@gmail.com>
2020-07-02 16:02:08 +0530

    HR jumping clouds problem

---
+++ HackerRank/jumping_clouds.py
@@ -0,0 +1,13 @@
+def jumpingOnClouds(c):
+    x,y = 0,0
+    while x<len(c)-2:
+        x = x+1 if c[x+2] else x+2
+        y+=1
+    if x<len(c)-1:
+        y+=1
+    return y
+
+
+if __name__ == '__main__':
+
+    print(jumpingOnClouds(list(map(int, '0 0 1 0 0 1 0'.rstrip().split()))))
